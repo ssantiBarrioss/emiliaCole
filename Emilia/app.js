cedb1f7d53b7060366a20c89765bebff3cb4ad3c
ssantiBarrioss <santiago.barrios@hotmail.es>
2023-10-12 22:13:38 -0300

    agrego los archivos html completos

--- Emilia/app.js
+++ Emilia/app.js
@@ -5,7 +5,15 @@ const path = require('path')
 app.get ('/',(req,res)=>{
     res.sendFile(path.join(__dirname,'/views/index.html'))
 })
-
+app.get ('/music',(req,res)=>{
+    res.sendFile(path.join(__dirname,'/views/music.html'))
+})
+app.get ('/about',(req,res)=>{
+    res.sendFile(path.join(__dirname,'/views/about.html'))
+})
+app.get ('/contact',(req,res)=>{
+    res.sendFile(path.join(__dirname,'/views/contact.html'))
+})
 app.use(express.static('public'))
 
 app.listen(3000,()=>{
